9961c998831e67f2857afb96b926da91814bb9ae
Paul Straw <paulstraw@paulstraw.com>
2023-11-09 00:27:08 -0800

    Sketching out Lua API

--- lua/pdportal.lua
+++ lua/pdportal.lua
@@ -1,8 +1,48 @@
-pdportal = {}
+import 'CoreLibs/object'
 
-local pdportal <const> = pdportal
+class("PdPortal").extends()
+local PdPortal <const> = PdPortal
 
--- TODO: What should this API even look like?
-pdportal.zed = function ()
-	-- TODO:
+local jsonEncode <const> = json.encode
+
+function PdPortal:sendToPeerConn(peerConnId, payload)
+	local jsonPayload = jsonEncode(payload)
+	print('') -- TODO: ASCII control char to indicate end of message?
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- These functions should be implemented by each app, and will be called by the
+-- pdportal web app automatically.
+
+pdpOnConnect = function(arg)
+	-- Called by pdportal web app after serial connection established.
+	-- TODO: How does PD know when serial disconnects?
+end
+
+pdpOnPeerServerOpen = function(peerId)
+	-- Peer server connection established, remote peer connection possible.
+	-- https://peerjs.com/docs/#peeron-open
+end
+
+pdpOnPeerServerConnection = function(connectionInfo)
+	-- A remote peer has conneted via the peer server.
+	-- { connectionStuff??? }
+	-- https://peerjs.com/docs/#peeron-connection
+end
+
+pdpOnPeerConnOpen = function(peerConnId)
+	-- Peer connection is usable
+	-- https://peerjs.com/docs/#dataconnection-on-open
+end
+
+pdpOnPeerConnClose = function(peerConnId)
+	-- Peer connection has been closed
+	-- https://peerjs.com/docs/#dataconnection-on-close
+end
+
+pdpOnPeerConnData = function(data)
+	-- data string is a JSON object of shape:
+	-- { peerConnId, payload }
+	-- https://peerjs.com/docs/#dataconnection-on-data
 end
